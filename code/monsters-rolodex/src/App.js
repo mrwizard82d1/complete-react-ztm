e2ac014e70c23f85928e63ededfb41d748e5a274
Larry Jones <mrwizard82d1@gmail.com>
2024-06-20 19:58:45 -0500

    Show a list of monsters (inefficiently)

--- code/monsters-rolodex/src/App.js
+++ code/monsters-rolodex/src/App.js
@@ -12,43 +12,24 @@ class App extends Component {
 
     // Initalize state with a JSON object
     this.state = {
-      // Ullustrating shallow merge
-      // Change name field to refer to an object
-      name: { firstName: 'Larry', lastName: 'Jones' },
-      company: 'Improving'
+      monster1: {
+        name: 'Linda'
+      },
+      monster2: {
+        name: 'Frank'
+      },
+      monster3: {
+        name: 'Jacky'
+      }
     }
   }
   
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi, { this.state.name.firstName } { this.state.name.lastName }! I work at { this.state.company }.</p>
-          <button onClick={() => {
-            // The advantage of this overload is that `setState` takes two arguments
-            // - state - The **current** (old) state
-            // - props - The propes of this component
-            this.setState(
-              // The first arguments is an updater **function**
-              () => {
-                // The value returned by this function is merged into the object, 
-                // `state`, **asynchronously**.
-                return {
-                  name: { firstName: 'Lawrence', lastName: 'Jones' }
-                }
-              }, 
-              // The second argument is an **optional** callback guaranteed to run **after** the first argument 
-              // has been run.
-              () => { 
-                console.log(this.state)
-              }
-            );
-          }}
-          >
-            Change name
-          </button>
-        </header>
+        <h1>{ this.state.monster1.name }</h1>
+        <h1>{ this.state.monster2.name }</h1>
+        <h1>{ this.state.monster3.name }</h1>
       </div>
     );
 }}
