38a88ed44e3c66712ddccc17c78b1df202458d93
Larry Jones <mrwizard82d1@gmail.com>
2024-07-02 21:30:07 -0500

    Filter monsters; reformat file
    Note that this implementation is **broken** because it expects an exact
    match for the filter; that is, one must type 'Lea' to filter out "Leanne
    ...." Entering 'lea' will filter out **all items** (because no items
    will match.)

--- code/monsters-rolodex/src/App.js
+++ code/monsters-rolodex/src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';  // The `Component` class provided by react
+import {Component} from 'react';  // The `Component` class provided by react
 
 import './App.css';
 
@@ -27,32 +27,41 @@ class App extends Component {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => this.setState(() => {
-        // This implementation completely replaces `this.state`
-        return { monsters: users }
-      },
-      // This function is invoked completely **after** the `fetch` returns (and has executed all apprepriate `thin` fragments)
-      () => { 
-        console.log(this.state)
-      }
-    ));
+          // This implementation completely replaces `this.state`
+          return {monsters: users}
+        },
+        // This function is invoked completely **after** the `fetch` returns (and has executed all apprepriate `thin` fragments)
+        () => {
+          console.log(this.state)
+        }
+      ));
   }
-  
+
   render() {
     console.log('Entering render()');
     return (
       <div className="App">
         <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
           console.log(event.target.value);
-          }}/>
+          const filteredMonsters = this.state.monsters.filter(monster => {
+            return monster.name.includes(event.target.value);
+          });
+
+          this.setState(() => {
+            return { monsters: filteredMonsters };
+          });
+        }}/>
+
         {
           this.state.monsters.map(monster => {
             // Using `monster.name` for key value works - but only because 
             // **we have no duplicate names**
-            return <div key={ monster.id }><h1>{ monster.name }</h1> </div>
+            return <div key={monster.id}><h1>{monster.name}</h1></div>
           })
         }
       </div>
     );
-}}
+  }
+}
 
 export default App
