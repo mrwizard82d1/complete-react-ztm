6165c7ef8f7a7fef641a56143f8991d74189d2b4
Larry Jones <mrwizard82d1@gmail.com>
2024-07-13 16:15:38 -0500

    Diagnose multiple calls to `CardList.render()`

--- code/monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ code/monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -3,9 +3,9 @@ import {Component} from 'react';
 class CardList extends Component {
 
   render() {
-    const {monsters} = this.props;
+    const { monsters } = this.props;
 
-    console.log('render');
+    console.log(`render ${ monsters.length }`);
 
     return (
       <div>
