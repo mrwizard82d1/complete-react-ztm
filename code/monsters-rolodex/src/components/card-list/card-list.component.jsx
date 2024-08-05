a154f997551b367a98c23b3912fd6e0d36dce059
Larry Jones <mrwizard82d1@gmail.com>
2024-08-05 16:27:15 -0500

    Generate details for each monsters
    - Name
    - Email

--- code/monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ code/monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -12,9 +12,13 @@ class CardList extends Component {
       <div className="card-list">
         {
           monsters.map(monster => (
-            <div className='card-container'>
+            <div className='card-container'
+                 key={monster.id}>
               {/* This code is rendered for each `monster` in `monsters`*/}
-              <img alt={`monster ${monster.name}`} src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} />
+              <img alt={`monster ${monster.name}`}
+                   src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} />
+              <h2>{monster.name}</h2>
+              <p>{monster.email}</p>
             </div>
           ))
         }
