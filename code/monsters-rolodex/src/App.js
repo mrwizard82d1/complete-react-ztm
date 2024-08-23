2810459cc4defa56f2b9e2c10ef26138cd29c92d
Larry Jones <mrwizard82d1@gmail.com>
2024-08-22 19:47:10 -0500

    Introduce functional components

--- code/monsters-rolodex/src/App.js
+++ code/monsters-rolodex/src/App.js
@@ -5,70 +5,83 @@ import SearchBox from "./components/search-box/search-box.component";
 
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    // Use constructor to instantiate **local** state
-
-    // **Must** be called to initialize parent state
-    super();  // Instantiates `Component` class
-
-    // Initialize state with a JSON object
-    this.state = {
-      monsters: [],
-      textToMatch: '',
-    };
-  }
-
-  componentDidMount() {
-
-    // Mounting occurs only the first time a component is added to the DOM
-
-    // Remember that `fetch` returns a `Promise` (that we will eventually get data)
-    // Convert HTTP response to usable form that
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => this.setState(() => {
-          // This implementation completely replaces `this.state`
-          return {monsters: users}
-        }
-      ));
-  }
-
-  onSearchChanged = (event) => {
-    // "Calculate" the current `textToMatch`
-    const textToMatch = event.target.value.toLocaleLowerCase();
-
-    // Save `textToMatch` in the state for use later
-    this.setState(() => {
-      // The expression `{ textToMatch }` translates to an object whose
-      // - Key is the name, `textToMatch` and whose
-      // - Value is the value of the variable, `textToMatch`
-      return {textToMatch};
-    });
-  };
-
-  render() {
-    console.log('App.render() called');
-
-    const { monsters, textToMatch } = this.state;
-    const { onSearchChanged } = this;
-
-    const filteredMonsters = monsters.filter(monster => {
-      const nameToTest = monster.name.toLocaleLowerCase();
-      return nameToTest.includes(textToMatch);
-    });
-
-    return (
-      <div className="App">
-        <h1 className='app-title'>Do the Monster Mash</h1>
-        <SearchBox
-          className='monsters-search-box'
-          placeholder="search monsters"
-          onChangeHandler={onSearchChanged} />
-        <CardList monsters={ filteredMonsters } />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className='app-title'>Do the Monster Mash</h1>
+      {/*<SearchBox*/}
+      {/*  className='monsters-search-box'*/}
+      {/*  placeholder="search monsters"*/}
+      {/*  onChangeHandler={onSearchChanged}/>*/}
+      {/*<CardList monsters={filteredMonsters}/>*/}
+    </div>
+  )
 }
+
+// class App extends Component {
+//   constructor() {
+//     // Use constructor to instantiate **local** state
+//
+//     // **Must** be called to initialize parent state
+//     super();  // Instantiates `Component` class
+//
+//     // Initialize state with a JSON object
+//     this.state = {
+//       monsters: [],
+//       textToMatch: '',
+//     };
+//   }
+//
+//   componentDidMount() {
+//
+//     // Mounting occurs only the first time a component is added to the DOM
+//
+//     // Remember that `fetch` returns a `Promise` (that we will eventually get data)
+//     // Convert HTTP response to usable form that
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then((response) => response.json())
+//       .then((users) => this.setState(() => {
+//           // This implementation completely replaces `this.state`
+//           return {monsters: users}
+//         }
+//       ));
+//   }
+//
+//   onSearchChanged = (event) => {
+//     // "Calculate" the current `textToMatch`
+//     const textToMatch = event.target.value.toLocaleLowerCase();
+//
+//     // Save `textToMatch` in the state for use later
+//     this.setState(() => {
+//       // The expression `{ textToMatch }` translates to an object whose
+//       // - Key is the name, `textToMatch` and whose
+//       // - Value is the value of the variable, `textToMatch`
+//       return {textToMatch};
+//     });
+//   };
+//
+//   render() {
+//     console.log('App.render() called');
+//
+//     const { monsters, textToMatch } = this.state;
+//     const { onSearchChanged } = this;
+//
+//     const filteredMonsters = monsters.filter(monster => {
+//       const nameToTest = monster.name.toLocaleLowerCase();
+//       return nameToTest.includes(textToMatch);
+//     });
+//
+//     return (
+//       <div className="App">
+//         <h1 className='app-title'>Do the Monster Mash</h1>
+//         <SearchBox
+//           className='monsters-search-box'
+//           placeholder="search monsters"
+//           onChangeHandler={onSearchChanged} />
+//         <CardList monsters={ filteredMonsters } />
+//       </div>
+//     );
+//   }
+// }
 
 export default App
---
+++ notes/complete-react-ztm/Function Components Intro.md
@@ -0,0 +1,17 @@
+- Functional components are an alternative to class components
+	- However, the general direction of React seems to be favoring functional components over class components
+	- In the wild, one will probably see **both**
+- Differences
+	- No constructor
+		- Just a JavaScript / TypeScript **function**
+	- No component methods
+		- Mount
+	- No `render`
+	- Functional components
+		- Simply run from top to bottom
+		- No **lifecycle methods**
+- Key ideas
+	- Functions
+	- Pure functions
+	- Impure functions / side effects
+	- 
