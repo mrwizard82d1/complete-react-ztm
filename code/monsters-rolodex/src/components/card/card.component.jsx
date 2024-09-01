eeca7ca51167d72d5139bbf4fad2c46caf17a857
Larry Jones <mrwizard82d1@gmail.com>
2024-08-31 20:29:51 -0500

    My refactoring from class to function
    But I did miss one change

--- code/monsters-rolodex/src/components/card/card.component.jsx
+++ code/monsters-rolodex/src/components/card/card.component.jsx
@@ -1,22 +1,18 @@
-import {Component} from 'react';
-
 import './card.styles.css';
 
-class Card extends Component {
-  render() {
-    const {id, name, email} = this.props.monster;
+const Card = (props) => {
+  const {id, name, email} = props.monster;
 
-    return (
-      <div className='card-container'
-           key={id}>
-        {/* This code is rendered for each `monster` in `monsters`*/}
-        <img alt={`monster ${name}`}
-             src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    )
-  }
+  return (
+    <div className='card-container'
+         key={id}>
+      {/* This code is rendered for each `monster` in `monsters`*/}
+      <img alt={`monster ${name}`}
+           src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  )
 }
 
 export default Card;
